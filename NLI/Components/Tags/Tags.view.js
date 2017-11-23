52937e1b6c52df4ca55db0e14281c0b342fdafec
FunkWiz <maoroz92@gmail.com>
2017-11-24 00:31:46 +0200

    font commit

---
+++ NLI/Components/Tags/Tags.view.js
@@ -0,0 +1,27 @@
+﻿import './Categories.scss'
+import Component from '../Abstracts/Component/Component'
+import CategoryList from './Components/CategoryList/CategoryList.view'
+import categories from '../../Api/categories.json';
+
+export default class Categories extends Component  {
+	constructor() {
+		super();
+		this.markup = 
+            `
+             <div class="categories">
+                <div class="categories-heading">
+                    <div class="categories-decade-info-wrp">
+                        <div class="categories-decade-info">
+                            <h4 class="categories-decade-title">40s</h4>
+                            <h4 class="categories-decade-count">433</h4>
+                        </div>
+                        <ul class="categories-decade-line-list">
+                            <li class="categories-decade-line-item" data-decade="1960"></li>
+                        </ul>
+                    </div>
+                    <div class="categories-content-wrp">${new CategoryList({items: categories.items}).getHtml()}</div>
+                </div>
+             </div>
+            `
+	}
+}
