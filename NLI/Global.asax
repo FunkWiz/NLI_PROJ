90a7186417ca3f822ed7e1183d369fcf11350c91
FunkWiz <maoroz92@gmail.com>
2017-11-23 12:00:17 +0200

    First project commit, no changes made

---
+++ NLI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NLI.MvcApplication" Language="C#" %>
---
+++ NLI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NLI.Startup))]
+namespace NLI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
