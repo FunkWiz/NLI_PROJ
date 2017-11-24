58381bdd9463581805cf0d75ea5053a3670ae137
FunkWiz <maoroz92@gmail.com>
2017-11-24 02:45:33 +0200

    new tag item fix

--- NLI/Components/Tags/Components/TagItem/TagItem.view.js
+++ NLI/Components/Tags/Components/TagItem/TagItem.view.js
@@ -2,17 +2,18 @@
 import Component from '../../../Abstracts/Component/Component'
 
 export default class TagItem extends Component  {
-	constructor(props) {
-	    super();
-	    this.title = props.title;
+    constructor(props) {
+        super();
+        this.title = props.title;
         this.value = props.value;
         this.markup = 
             `
              <li class="tag-item" data-tag="${this.value}">
+                <div class="tag-item-image"></div>
                 <div class="tag-item-content-wrp">
-                    <label class="tag-item-title">${this.title}</label>
+                    <label class="tag-item-title">#${this.title}</label>
                 </div>
              </li>
             `
-	}
+    }
 }
